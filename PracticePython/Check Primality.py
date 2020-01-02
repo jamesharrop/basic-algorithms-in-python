1fc84d913bfe91e8f4934938bd2f8425eb649a8d
name <20360366+jamesharrop@users.noreply.github.com>
2020-01-02 20:28:02 +0000

    more prime number exercises

--- PracticePython/Check Primality.py
+++ PracticePython/Check Primality.py
@@ -1,5 +1,6 @@
 ''' Exercise 11 (and Solution)
 Ask the user for a number and determine whether the number is prime or not.
+The definition of a prime number is a positive integer that has exactly two positive divisors
 '''
 
 num = int(input("Enter a number: "))
@@ -17,14 +18,26 @@ if is_prime(num):
 else:
     print(f"{num} is not a prime number.")
 
+def check_num_is_not_below_one(func):
+    def inner(num):
+        if num<1:
+            print("Can't calculate that")
+            return None
+        return func(num)
+    return inner
+
+@check_num_is_not_below_one # A decorator is not the best way to structure this here
 def is_prime_faster(num): # It stops looking once it finds any factor
     value = 1
+    if num == 1:
+        return False
     while True:
         value += 1
-        if num % value == 0:
-            return(False)
-        if value == int(num/2)+1:
+        if value > int(num ** 0.5):
             return(True)
+        if num % value == 0:
+            print(f'{int(num/value)} x {value} = {num}')
+            return(False)
 
 print("Try another method:")
 
